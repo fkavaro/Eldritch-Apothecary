d9fbc067697a98f4f8e455c100b05382faf11efe
fkavaro <by.fka.varo@gmail.com>
2025-02-12 17:15:29 +0100

    Fixing materials fot URP

---
+++ Eldritch Apothecary/Assets/Scripts/Client_BehaviourRunner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourAPI.Core;
+using BehaviourAPI.Core.Actions;
+using BehaviourAPI.Core.Perceptions;
+using BehaviourAPI.UnityToolkit;
+using BehaviourAPI.StateMachines;
+
+public class Client_BehaviourRunner : BehaviourRunner
+{
+
+	protected override BehaviourGraph CreateGraph()
+	{
+		FSM Client_FSM = new FSM();
+
+		State Waiting = Client_FSM.CreateState();
+
+		return Client_FSM;
+	}
+}
