aa2730f167c4f9f979075f78199b9a32c2aec0bc
fkavaro <by.fka.varo@gmail.com>
2025-02-13 16:22:24 +0100

    Few transitions added in ClientFSM

--- Eldritch Apothecary/Assets/Scripts/Client_BehaviourRunner.cs
+++ Eldritch Apothecary/Assets/Scripts/Client_BehaviourRunner.cs
@@ -8,15 +8,28 @@ using BehaviourAPI.UnityToolkit;
 using BehaviourAPI.StateMachines;
 using BehaviourAPI.UnityToolkit.GUIDesigner.Runtime;
 
+public enum Service
+{
+	None,
+	Sorcerer,
+	Alchemist
+}
+
 public class Client_BehaviourRunner : BehaviourRunner
 {
 	#region VARIABLES
-	[Tooltip("Maximum minutes able to wait"), Range(2, 10)]
-	public int patience = 2;
-	[Tooltip("Maximum number of scares sopported"), Range(2, 5)]
-	public int maxScares = 2;
+	[Header("Client Properties")]
+	[Tooltip("Desired service to be attended")]
+	public Service wantedService;
+	[Tooltip("Maximum minutes willing to wait"), Range(2, 10)]
+	public int maxMinutesWaiting = 2;
+	[Tooltip("Probability of being scared"), Range(0, 10)]
+	public int scareProbability = 3;
+	[Tooltip("Maximum number of scares supported"), Range(1, 5)]
+	public int maxScares = 3;
 	#endregion
 
+	int _scaresCount = 0;
 	Collider _visionCollider;
 	Transform _cat;
 	BSRuntimeDebugger _debugger;
@@ -24,6 +37,10 @@ public class Client_BehaviourRunner : BehaviourRunner
 	protected override void Init()
 	{
 		_debugger = GetComponent<BSRuntimeDebugger>();
+		wantedService = (Service)UnityEngine.Random.Range(0, 3); // Chooses a service randomly
+		maxMinutesWaiting = UnityEngine.Random.Range(2, 11); // Chooses a random number of minutes to wait
+		scareProbability = UnityEngine.Random.Range(0, 11); // Chooses a random scare probability
+		maxScares = UnityEngine.Random.Range(1, 6); // Chooses a random number of supported scares
 		base.Init(); // Calls the base class method
 	}
 
@@ -39,23 +56,21 @@ public class Client_BehaviourRunner : BehaviourRunner
 		// Waits in line to be attended by the receptionist 
 		State WaitingForReceptionist = clientFSM.CreateState("WaitingForReceptionist");
 		// Attended by the receptionist
-		State AttendedByReceptionist = clientFSM.CreateState("AttendedByReceptionist");
+		State TalkingToReceptionist = clientFSM.CreateState("AttendedByReceptionist");
 		// Complains to the receptionist if waited too much time
-		State ComplainingToReceptionist = clientFSM.CreateState("ComplainingToReceptionist");
+		State Complaining = clientFSM.CreateState("Complaining");
+		// Waits sat down to be attended by the sorcerer
+		State WaitingForService = clientFSM.CreateState("WaitingForService");
 
 		// a. SORCERER
-		// Waits sat down to be attended by the sorcerer
-		State WaitingForSorcerer = clientFSM.CreateState("WaitingForSorcerer");
 		// Goes to the sorcerer's room
-		State GoingToSorcererRoom = clientFSM.CreateState("GoingToSorcererRoom");
+		//State GoingToSorcererRoom = clientFSM.CreateState("GoingToSorcererRoom");
 		// Attended by sorcerer
 		State VisitingSorcerer = clientFSM.CreateState("VisitingSorcerer");
 
-		// b. ALCHEMIST		
-		// Waits sat down while potion is made by the alchemist
-		State WaitingForPotion = clientFSM.CreateState("WaitingForSorcerer");
+		// b. ALCHEMIST
 		// Takes the prepared potion
-		State TakingPotion = clientFSM.CreateState("TakingPotion");
+		State CollectingPotion = clientFSM.CreateState("TakingPotion");
 
 		// Leaves the apothecary | Cancels the purchase if stunned too many times by the cat
 		State Leaving = clientFSM.CreateState("Leaving");
@@ -64,33 +79,81 @@ public class Client_BehaviourRunner : BehaviourRunner
 		clientFSM.SetEntryState(Shopping);
 		#endregion
 
-		#region PULL ERCEPTIONS
-		ConditionPerception watchPlayer = new(CheckWatchCat);
+		#region PULL PERCEPTIONS
+		ConditionPerception stunnedByCat = new(ReactToCat);
+		UnityTimePerception desperate = new(maxMinutesWaiting * 60); // From minutes to seconds
+
+		// Turn for receptionist
+		//ConditionPerception receptionistTurn = new(CheckReceptionistTurn);
+		// Something else (bool)
+		ConditionPerception anyServiceIsWanted = new(CheckIfWantedService);
+		// Turn for sorcerer
+		ConditionPerception sorcererTurn = new(CheckSorcererTurn);
+		// Potion ready
+		ConditionPerception isPotionReady = new(IsPotionReady);
+		#endregion
+
+		#region PUSH PERCEPTIONS
+
 		#endregion
 
 		#region TRANSITIONS
+		clientFSM.CreateTransition("Shopping -> WaitingForReceptionist", Shopping, WaitingForReceptionist, statusFlags: StatusFlags.Finished);
+		clientFSM.CreateTransition("WaitingForReceptionist -> TalkingToReceptionist", WaitingForReceptionist, TalkingToReceptionist, statusFlags: StatusFlags.Finished);
+		clientFSM.CreateTransition("TalkingToReceptionist -> WaitingForService", TalkingToReceptionist, WaitingForService, anyServiceIsWanted);
+		clientFSM.CreateTransition("TalkingToReceptionist -> Leaving", TalkingToReceptionist, Leaving, anyServiceIsWanted); //!FIX: !anyServiceIsWanted
+		clientFSM.CreateTransition("WaitingForService -> VisitingSorcerer", WaitingForService, VisitingSorcerer, sorcererTurn);
+		clientFSM.CreateTransition("WaitingForService -> CollectingPotion", WaitingForService, CollectingPotion, isPotionReady);
+		clientFSM.CreateTransition("VisitingSorcerer -> Leaving", VisitingSorcerer, Leaving, statusFlags: StatusFlags.Finished);
+		clientFSM.CreateTransition("CollectingPotion -> Leaving", CollectingPotion, Leaving, statusFlags: StatusFlags.Finished);
+		// TODO: StunnedByCat transitions FROM ALL with StackFSM?
 		#endregion
 
-		_debugger.RegisterGraph(clientFSM);
+		_debugger.RegisterGraph(clientFSM, "Client_FSM");
 		return clientFSM;
 	}
 
-	/// <summary>
-	/// Checks if the client is startled by the cat
-	/// </summary>
-	private bool CheckWatchCat()
+	private bool CheckReceptionistTurn()
 	{
-		// TODO: Check colission by tag. //////////////////
+		throw new NotImplementedException();
+	}
+
+	/// <summary>
+	/// Checks if the client wants the sorcerer or alchemist service
+	/// </summary>
+	/// <returns></returns>
+	private bool CheckIfWantedService()
+	{
+		return wantedService == Service.Sorcerer || wantedService == Service.Alchemist;
+	}
+
+	private bool CheckSorcererTurn()
+	{
+		throw new NotImplementedException();
+	}
+	private bool IsPotionReady()
+	{
+		throw new NotImplementedException();
+	}
+
+	/// <summary>
+	/// Checks if the client is stunned by the cat
+	/// </summary>
+	private bool ReactToCat()
+	{
+		// TODO: Check colission with tag //////////////////
 		if (_visionCollider.bounds.Contains(_cat.position))
 		{
 			Vector3 direction = (_cat.position - transform.position).normalized;
 			Ray ray = new Ray(transform.position + transform.up, direction * 20);
 
-			bool watchPlayer = Physics.Raycast(ray, out RaycastHit hit, 20) && hit.collider.gameObject.transform == _cat;
+			bool watchingCat = Physics.Raycast(ray, out RaycastHit hit, 20) && hit.collider.gameObject.transform == _cat;
 
-			return watchPlayer;
+			if (watchingCat)
+				// Calculate random number and check if it's less than fear property
+				if (UnityEngine.Random.Range(0, 10) < scareProbability)
+					return true;
 		}
 		return false;
-		//////////////////////////////////////////////////
 	}
 }
