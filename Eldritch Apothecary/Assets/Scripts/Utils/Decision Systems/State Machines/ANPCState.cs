e905ade165049ed78c1fd13b7bf62feb426a6212
fkavaro <by.fka.varo@gmail.com>
2025-04-18 20:19:02 +0200

    Update ANPCState.cs

--- Eldritch Apothecary/Assets/Scripts/Utils/Decision Systems/State Machines/ANPCState.cs
+++ Eldritch Apothecary/Assets/Scripts/Utils/Decision Systems/State Machines/ANPCState.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Base class for NPC states, allowing to handle animations.
+/// </summary>
+/// <typeparam name="TController"></typeparam>
+/// <typeparam name="TStateMachine"></typeparam>
 public abstract class ANPCState<TController, TStateMachine> : AState<TController, TStateMachine>
     where TController : ANPC<TController>
     where TStateMachine : AStateMachine<TController, TStateMachine>
@@ -8,18 +13,15 @@ public abstract class ANPCState<TController, TStateMachine> : AState<TController
     public ANPCState(string name, TStateMachine stateMachine) : base(name, stateMachine) { }
 
     /// <summary>
-    /// Coroutine to wait for a specified amount of time before switching to the next state.
+    /// Coroutine to wait for a specified amount of time playing an animation before switching to the next state.
     /// </summary>
-    protected IEnumerator WaitAndSwitchState(float waitTime, AState<TController, TStateMachine> nextState, int animation = -1, string action = "Executing animation")
+    protected IEnumerator WaitAndSwitchState(float waitTime, AState<TController, TStateMachine> nextState, int animation, string animationName)
     {
         _coroutineStarted = true;
 
-        _behaviourController.actionText.text = action + " for " + waitTime + " seconds...";
+        _behaviourController.actionText.text = animationName + " for " + waitTime + " seconds...";
 
-        if (animation != -1)
-        {
-            _behaviourController.ChangeAnimationTo(animation);
-        }
+        _behaviourController.ChangeAnimationTo(animation);
 
         yield return new WaitForSeconds(waitTime);
 
@@ -28,11 +30,11 @@ public abstract class ANPCState<TController, TStateMachine> : AState<TController
         _coroutineStarted = false;
     }
     /// <summary>
-    /// Coroutine to wait for a random amount of time before switching to the next state.
+    /// Coroutine to wait for a random amount of time playing an animation before switching to the next state.
     /// </summary>
-    protected IEnumerator WaitAndSwitchState(AState<TController, TStateMachine> nextState, int animation = -1, string action = "Acting")
+    protected IEnumerator WaitAndSwitchState(AState<TController, TStateMachine> nextState, int animation, string animationName)
     {
         int waitTime = Random.Range(5, 21);
-        return WaitAndSwitchState(waitTime, nextState, animation, action);
+        return WaitAndSwitchState(waitTime, nextState, animation, animationName);
     }
 }
